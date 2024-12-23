6e7478e9fbb3b825b2ee5ed6bc7099bd947cd106
Sonu Verma <sonu.verma0715@gmail.com>
2024-12-23 22:29:22 +0530

    search related changes done for restaurant

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,22 +6,13 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
 
-    let filteredRestoCards = [];
-    let restaurantList = [];
-
-    
-    if(restoData.data.cards.length > 0){
-         filteredRestoCards = restoData?.data?.cards?.filter(
-            (appData) => !isEmpty(appData) && appData.card.card.id === 'restaurant_grid_listing'
-        ) || [];
-    }
-
-    if(filteredRestoCards.length > 0){
-        restaurantList = filteredRestoCards[0].card.card.gridElements.infoWithStyle.restaurants;
-    }
-    
-
     const [restaurants, setRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]); 
+    const [searchText, setSearchText] = useState("");
+    const [isFiltered, setIsFiltered] = useState(false);
+
+    const [showSearchText, setShowSearchText] = useState("");
+    const [filteredParam, setFilteredParam] = useState([]);
 
     useEffect(()=> {
         fetchRestoData();
@@ -32,56 +23,107 @@ const Body = () => {
 
         const json =await data.json();
 
-        setRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
 
-    if(restaurants.length == 0){
-        return <>
-            <div className='body'>
-                <div style={{ display:"flex", justifyContent: "space-between"}}>
-                <div className='search'>
-                        <input type='text' placeholder='Search' className='search-input' />
-                        <button className='search-botton'>Search</button>
-                </div>
-                <div className="filter">
-                        <button  className='search-botton' 
-                        onClick={
-                            () => { 
-                                    setRestaurants(restaurants.filter( (restaurant) => restaurant.info.avgRating > 4.3 ))
-                                }
-                            }
-                        >Top Rating Restaurant</button>
-                </div>
-                </div>
-                <Shimmer />
-            </div>
-        </>
-    }
+    
+    /* Used for filter from api */
+
+    // let filteredRestoCards = [];
+    // let restaurantList = [];
+
+    
+    // if(restoData.data.cards.length > 0){
+    //      filteredRestoCards = restoData?.data?.cards?.filter(
+    //         (appData) => !isEmpty(appData) && appData.card.card.id === 'restaurant_grid_listing'
+    //     ) || [];
+    // }
+
+    // if(filteredRestoCards.length > 0){
+    //     restaurantList = filteredRestoCards[0].card.card.gridElements.infoWithStyle.restaurants;
+    // }
+
+    /* Used for filter from api */
+
     return (
         <div className='body'>
             <div style={{ display:"flex", justifyContent: "space-between"}}>
                <div className='search'>
-                    <input type='text' placeholder='Search' className='search-input' />
-                    <button className='search-botton'>Search</button>
+                    <input type='text' placeholder='Search' className='search-input' value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+                    <button 
+                        className='search-botton' 
+                        onClick={
+                                () => { 
+                                        if(searchText.length > 0){
+                                            const filterRestaurants  = restaurants.filter(restaurant => restaurant?.info?.name?.toLowerCase().includes(searchText.toLowerCase()));
+                                            setFilteredRestaurants(filterRestaurants);
+                                            setIsFiltered(true);
+                                            setShowSearchText(searchText);
+
+                                            // Add searchText to filteredParam if not already added
+                                            setFilteredParam(pre => {
+                                                if(searchText == "Top Ratings"){
+                                                    return []
+                                                }
+                                                if(!pre.includes(searchText)){
+                                                    return [...pre, searchText]
+                                                }
+                                                return pre
+                                            })
+                                        }
+                                       
+                                    }
+                            } 
+                    >Search</button>
+                    {
+                        isFiltered ? 
+                        <span className="clearedFilter"><span className="searchTextSpan">{ filteredParam.length >0 ? filteredParam.join(", "): '' } {console.log("filteredParam",filteredParam) }</span>
+                            <button onClick={ (e) => {
+                            e.preventDefault();
+                            setSearchText('')
+                            fetchRestoData()
+                            setIsFiltered(false);
+                            setFilteredParam([])
+                        }}>X</button></span>
+                        : ''
+                    }
                </div>
                <div className="filter">
                     <button  className='search-botton' 
                     onClick={
                         () => { 
-                                setRestaurants(restaurants.filter( (restaurant) => restaurant.info.avgRating > 4.3 ))
+                                const filterRestaurants  = filteredRestaurants.filter( 
+                                    (restaurant) => restaurant.info.avgRating > 4.0 
+                                );
+                                setFilteredRestaurants(filterRestaurants)
+                                setIsFiltered(true);
+                                setShowSearchText("Top Ratings");
+                                setFilteredParam(pre => {
+                                    if(!pre.includes("Top Ratings")){
+                                        return [...pre, "Top Ratings"]
+                                    }
+
+                                    return pre
+                                })
                             }
                         }
                     >Top Rating Restaurant</button>
                </div>
             </div>
-            <div className='resto-container'>
-              {
-                restaurants.map((restaurant, index) =>
-                    //  console.log(restaurant.info.)
-                    <RestoCard key={restaurant.info.id} resto={restaurant.info} /> 
+            {
+                filteredRestaurants.length === 0 && !isFiltered ? <Shimmer /> : filteredRestaurants.length === 0 && isFiltered ? 'No Data found' : (
+                    <div className='resto-container'>
+                    {
+                        filteredRestaurants.map((restaurant, index) =>
+                            //  console.log(restaurant.info.)
+                            <RestoCard key={restaurant.info.id} resto={restaurant.info} /> 
+                        )
+                    }
+                    </div>
                 )
-              }
-            </div>
+            }
+            
         </div>
     );
 }
